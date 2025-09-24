59f79785d725a0ade08963afd2b4be2b6f16a9ef
Masoumeh Molaei <masoumeh@Masoumehs-Laptop.local>
2025-09-24 14:45:26 +0330

    Adding portfolio analysis

--- portfolio/analysis.py
+++ portfolio/analysis.py
@@ -37,6 +37,7 @@ class LoadData():
         plt.clf()
         return base64.b64encode(buffer.getvalue()).decode('utf-8')
 
+
 class Statistics():
     return_data = pd.DataFrame()
 
@@ -115,6 +116,7 @@ class Statistics():
         cointegrated = [sym for sym, p in results if p < 0.05]
         return cointegrated
 
+
 class PortfolioAnalysis():
     window_size = 1
     johansen_weights = pd.DataFrame()
@@ -167,7 +169,7 @@ class PortfolioAnalysis():
                 })
 
         weights_df = pd.DataFrame(weight_series)
-        weights_df['date'] = pd.to_datetime(weights_df['date']).dt.date
+        # weights_df['date'] = pd.to_datetime(weights_df['date']).dt.date
         self.johansen_weights = weights_df
 
     def build_portfolio_series(self):
@@ -188,3 +190,101 @@ class PortfolioAnalysis():
         value_df['return'] = value_df['price'].pct_change()
         self.portfolio_price = value_df[['date', 'price']]
         self.portfolio_return = value_df[['date', 'return']].dropna()
+
+    def plot_return(self):
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.portfolio_return)
+        plt.title("Portfolio Return", fontsize=24)
+        plt.xlabel("Date", fontsize=12)
+        plt.ylabel("Return", fontsize=12)
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+        plt.xticks(rotation=30)
+
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        plt.clf()
+        return base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    def plot_cumulative_return(self):
+        cum_returns = self.portfolio_return.cumsum()
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(cum_returns)
+        plt.title("Cumulative Portfolio Return", fontsize=24)
+        plt.xlabel("Date", fontsize=12)
+        plt.ylabel("Cumulative Return", fontsize=12)
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+        plt.xticks(rotation=30)
+
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        plt.clf()
+        return base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    def plot_portfolio_return_histogram(self):
+        plt.figure(figsize=(10,5))
+        plt.hist(self.portfolio_return, bins=50, edgecolor='blue')
+        plt.title('Portfolio Return Histogram', fontsize=12)
+        plt.xlabel('Portfolio Return', fontsize=12)
+        # plt.ylabel('Frequency', fontsize=14)
+        plt.grid(axis='y')
+
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        plt.clf()
+        return base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+
+class RiskIndicators():
+    return_data = pd.DataFrame()
+    max_drawdown_percentage = 0
+    standard_deviation = 0
+    sortino_ratio = 0
+    value_at_risk = 0
+    sharpe_ratio = 0
+
+    def __init__(self, return_data):
+        self.return_data = return_data
+
+    def get_sharpe_ratio(self, asset, risk_free_rate=0.04, days=255):
+        mean = asset.mean()
+        std = asset.std()
+        sharpe_ratio = ((mean - risk_free_rate / days) / std) * np.sqrt(days)
+        self.sharpe_ratio = sharpe_ratio
+
+    def get_max_drawdown(self, asset):
+        cumulative_max = asset.cummax()
+        drawdown = (asset - cumulative_max) / cumulative_max
+        max_drawdown = drawdown.min()
+        max_drawdown_percentage = max_drawdown * 100
+        self.max_drawdown_percentage = max_drawdown_percentage
+
+    def get_sortino_ratio(self, asset, risk_free_rate=0.04, days=255):
+        down_side_returns = asset[asset < 0]
+        mean = asset.mean()
+        std = down_side_returns.std()
+        sortino_ratio = ((mean - risk_free_rate / days) / std) * np.sqrt(days)
+        self. sortino_ratio = sortino_ratio
+
+    def portfolio_std_dev(self, weights):
+        returns = self.return_data
+        weights = np.array(weights)
+        portfolio = np.array([returns['Index'].values, returns['Foolad'].values, returns['Fazar'].values])
+        cov_matrix = np.cov(portfolio)
+        portfolio_variance = np.dot(weights.T, np.dot(cov_matrix, weights))
+        portfolio_std_dev = np.sqrt(portfolio_variance)
+        self.standard_deviation = portfolio_std_dev
+
+    def get_VaR(self, confidence_level=0.95):
+        returns = self.return_data
+        num_assets = 6
+        weights = np.repeat(1 / num_assets, num_assets)
+        portfolio_returns = returns.mul(weights)
+        mean = portfolio_returns.mean()
+        std_dev = portfolio_returns.std()
+        z_score = st.norm.ppf(1 - (1 - confidence_level))
+        VaR = mean - (z_score * std_dev)
+        self.value_at_risk = VaR
